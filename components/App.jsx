7f86a30ba943644b789ad4db8434f34d8c2e0538
Abhishek <abhishek.tanwar@cloverventures.in>
2024-01-08 20:39:33 +0530

    AT | tailwind css setup

---
+++ components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = ()=> {
+  return <h3 className='text-3xl font-bold underline'>Cashfree Payments Dashboard</h3>
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend:{
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      },
+      colors: {
+        primary: '#493932'
+      },
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+}
+
